8c5ab95630b978da93d97301151dfa816e1b237c
Justas Balcas <juztas@gmail.com>
2026-05-18 13:27:00 -0500

    Add MX/PTX commands

--- test-runner.py
+++ test-runner.py
@@ -15,12 +15,13 @@ TODO:
 """
 import os
 import json
-import yaml
 import pprint
+import yaml
 import ansible_runner
 
 
 def getInventory(inventoryFile):
+    """Get inventory as dict"""
     with open(inventoryFile, 'r', encoding='utf-8') as fd:
         out = yaml.safe_load(fd.read())
     return out
@@ -33,18 +34,19 @@ def runAnsible(playbookFile):
     return ansRunner
 
 def saveOutput(inJson, filename, outputdir):
+    """Save output json and config to file"""
     try:
         os.makedirs(os.path.join(outputdir, 'json/'), exist_ok = True)
         os.makedirs(os.path.join(outputdir, 'config/'), exist_ok = True)
     except OSError as ex:
-        print("Directory '%s' can not be created" % outputdir)
+        print(f"Directory '{outputdir}' can not be created")
         raise Exception(ex) from OSError
     # Dump Json
-    fname = os.path.join(outputdir, "json/", "%s.json" % filename)
+    fname = os.path.join(outputdir, "json/", f"{filename}.json" )
     with open(fname, 'w', encoding='utf-8') as fd:
         json.dump(inJson, fd)
     # Save config in txt format
-    fname = os.path.join(outputdir, "config/", "%s.config" % filename)
+    fname = os.path.join(outputdir, "config/", f"{filename}.config")
     with open(fname, 'w', encoding='utf-8') as fd:
         fd.writelines(inJson['ansible_net_config'])
 
@@ -52,7 +54,7 @@ def saveOutput(inJson, filename, outputdir):
 outputdir="/opt/siterm/config/ansible/sense"
 playbooks = ['getfacts.yaml', 'applyconfig.yaml']
 for playbook in playbooks:
-    print("RUNNING PLAYBOOK: %s" % playbook)
+    print(f"RUNNING PLAYBOOK: {playbook}")
     r = runAnsible(playbook)
     for host, _ in r.stats['failures'].items():
         for host_events in r.host_events(host):
@@ -60,7 +62,7 @@ for playbook in playbooks:
                 continue
             pprint.pprint(host_events)
     for host, _ in r.stats['ok'].items():
-        print("HOSTNAME: %s" % host)
+        print(f"HOSTNAME: {host}")
         print('-'*100)
         for host_events in r.host_events(host):
             if host_events['event'] != 'runner_on_ok':
@@ -73,6 +75,18 @@ for playbook in playbooks:
             elif 'ansible_facts' in host_events['event_data']['res'] and  \
                  'ansible_net_interfaces' in host_events['event_data']['res']['ansible_facts']:
                 pprint.pprint(host_events['event_data']['res']['ansible_facts'])
+            elif 'ansible_facts_file' in host_events['event_data']['res'] and \
+                 'file' in host_events['event_data']['res']['ansible_facts_file']:
+                fname = host_events['event_data']['res']['ansible_facts_file']['file']
+                if os.path.exists(fname):
+                    try:
+                        with open(fname, "r", encoding="utf-8") as f:
+                            facts_data = json.load(f)
+                            pprint.pprint(facts_data)
+                    except json.JSONDecodeError as e:
+                        print(f"[ERROR] Could not parse JSON from {fname}: {e}")
+                else:
+                    print(f"[WARNING] Facts file not found: {fname}")
             else:
                 pprint.pprint(host_events)
             if playbook == 'getfacts.yaml':
